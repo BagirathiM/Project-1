cc00072413bdeabc7b5b4a38bbb884175c95305c
BagirathiM <bagirathi2003@gmail.com>
2025-03-23 21:39:45 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,68 @@
+<header>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a href="#" class="logo navbar-brand">
+        <img src="./images/img-100x80-logo.webp" alt="logo" width="100" height="60" loading="lazy" class="d-inline-block align-center">
+      </a>
+       
+      <!-- Hamburger Menu Button -->
+      <button
+        class="navbar-toggler menu-btn"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#myNavBar"
+        aria-controls="myNavBar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Close Button (Initially Hidden) -->
+      <button
+        class="navbar-toggler close-btn d-none"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#myNavBar"
+        aria-controls="myNavBar"
+        aria-expanded="true"
+        aria-label="Close navigation">
+     
+        <span class="text-secondary">✖</span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="myNavBar">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item me-4 active">
+            <a href="#" class="nav-link">HOME</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#aboutus" class="nav-link">ABOUT US</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#brands" class="nav-link">BRANDS</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#category" class="nav-link">CATEGORIES</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#services" class="nav-link">SERVICES</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#products" class="nav-link">PRODUCTS</a>
+          </li>
+          <li class="nav-item me-4">
+            <a href="#testimonial" class="nav-link">TESTIMONIALS</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="address">
+    <i class="fa-solid fa-location-dot fs-2 ms-3 me-3 pt-3"></i>
+    <address class="custom-address pt-3 fs-5 text-center">93, Opposite L.I.C Bus Stop, Trivandrum Road,
+      Palayamkottai, Tirunelveli, Tamil Nadu - 627 002.</address>
+  </div>
+</header>
+<main>
